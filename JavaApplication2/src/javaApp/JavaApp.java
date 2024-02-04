d4f5c77629af0ffc5adc1fb0d9eef956edab77b7
emitsuwu <ccugwuegbulam@gmail.com>
2024-02-03 18:30:13 -0600

    changed the name of the first project

--- JavaApplication2/src/javaApp/JavaApp.java
+++ JavaApplication2/src/javaApp/JavaApp.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication2;
+package javaApp;
 
 /**
  *
  * @author Test
  */
-public class JavaApplication2 {
+public class JavaApp {
 
     /**
      * @param args the command line arguments
